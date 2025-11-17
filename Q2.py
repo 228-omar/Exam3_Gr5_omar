8ec89ffd5f6d70bbd04be1d99e4977f744603f34
omar <2287196@cegepoutaouais.qc.ca>
2025-11-17 10:06:43 -0500

    exam

--- Q2.py
+++ Q2.py
@@ -2,22 +2,30 @@ import datetime
 import locale
 locale.setlocale(locale.LC_TIME,'')
 
-def afficher_jours_examens(horaire_examen: dict) -> list[str]:
+
+
+def afficher_jours_examens(p_horaire_examen: dict) -> list:
     """
     Cette fonction sert à extraire les jours de la semaines où il y a des examens
-    :param horaire_examen: dictionnaire contenant les dates d'examens
+    :param p_horaire_examen: dictionnaire contenant les dates d'examens
     :return: une liste de jours de la semaine
     """
-    for i in range(len(horaire_examen)):
-        jours = []
-        date = datetime.datetime.strptime(horaire_examen[i], "%Y-%m-%d")
-        j = date.strftime("%a")
-        jours.append(j)
-        return jours
+    for  value in p_horaire_examen.items():
+            jours = []
+            date_examen = datetime.datetime.strptime(horaire_examen[value], "%Y-%m-%d")
+            j = date_examen.strftime("%A")
+            jours.append(j)
+            print(f"Les examens sont {jours} ")
+            print(jours)
+    return jours
+
+
 
 if __name__ == '__main__':
+
+
     horaire_examen = {
-        "math" : "10/12/2015",
+        "math" : "10/12/2025",
         "anglais" : "12/12/2025",
         "français" : "15/12/2025"
     }
